a9f94e5d01dc0f62da27fac5450d4904a43489b4
Beloded Darya <dariabeloded@gmail.com>
2019-05-26 13:00:08 +0300

    oijerfor

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const port = process.env.PORT || 8000;
 // systeminfo 
 // 192.168.0.14
 const server = app.listen (port, () => { 
-  console.log ('Экспресс-сервер прослушивает порт' + port); 
+  console.log ('Экспресс-сервер прослушивает порт ' + port); 
 });
